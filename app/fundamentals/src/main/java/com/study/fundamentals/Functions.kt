996fbca8bcba3cfd52f7a28376303af4d2e6f6d9
Daniel Melo <daniel.melo42@outlook.com>
2022-11-17 18:46:54 -0700

    Functions default values

--- app/fundamentals/src/main/java/com/study/fundamentals/Functions.kt
+++ app/fundamentals/src/main/java/com/study/fundamentals/Functions.kt
@@ -1,11 +1,11 @@
 package com.study.fundamentals
 
 fun main() {
-    calculate(start = 2, end = 20, modulo = 6)
+    calculate(start = 2, end = 20, modulo = 6, message = "is a multiple of")
 }
 
-fun calculate(start: Int = 1, end:Int = 100, modulo: Int = 2) {
+fun calculate(start: Int = 1, end:Int = 100, modulo: Int = 2, message: String = "is divisible by") {
     for (i in start..end) {
-        if (i % modulo == 0) println("number $i is a multiple of $modulo")
+        if (i % modulo == 0) println("number $i $message $modulo")
     }
 }
