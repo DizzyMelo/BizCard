ca360a918803a32879a7c097c223d45698e34517
Daniel Melo <daniel.melo42@outlook.com>
2022-11-17 19:54:08 -0700

    Functions return values

--- app/fundamentals/src/main/java/com/study/fundamentals/Functions.kt
+++ app/fundamentals/src/main/java/com/study/fundamentals/Functions.kt
@@ -2,7 +2,19 @@ package com.study.fundamentals
 
 fun main() {
     calculate(start = 2, end = 20, modulo = 6, message = "is a multiple of")
+    val catAge = calculateCatAge(age = 20)
+
+    if (checkAge(age = catAge)) {
+        println("The cat is old")
+    } else {
+        println("The cat is young")
+    }
+    println(checkAge(age = catAge))
 }
+
+fun calculateCatAge(age: Int): Int = age * 7
+
+fun checkAge(age: Int): Boolean = age > 60
 
 fun calculate(start: Int = 1, end:Int = 100, modulo: Int = 2, message: String = "is divisible by") {
     for (i in start..end) {
