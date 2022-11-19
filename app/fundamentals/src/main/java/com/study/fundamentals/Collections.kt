3c36b7d076d0a1ca8042523d94a56d33a02f745c
Daniel Melo <daniel.melo42@outlook.com>
2022-11-18 17:40:23 -0700

    Collections filter

--- app/fundamentals/src/main/java/com/study/fundamentals/Collections.kt
+++ app/fundamentals/src/main/java/com/study/fundamentals/Collections.kt
@@ -3,13 +3,20 @@ package com.study.fundamentals
 fun main() {
 
 //    val names = listOf("Daniel", "Kaynara", "May", "Gina")
-    val names = mutableListOf(1,3,5,6,3,)
-    names.add(0, 34564)
+    val names = mutableListOf("Daniel", "Kay", "Matt")
+    names.add(0, "Jose")
     names.removeAt(3)
     println("Number of elements is ${names.size}")
     println("Second of elements is ${names[1]}")
-    println("Index of element 6 is ${names.indexOf(6)}")
+    println("Index of element 6 is ${names.indexOf("Daniel")}")
     println(names)
+
+    val found = names.filter {
+        it.length == 4
+    }
+
+    println("elements")
+    println(found)
 
     val cities = mutableListOf<String>()
     val empty = emptyList<String>()
